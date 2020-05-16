8ee01de2f7d3f065fe35983e8c594711f01cff77
Kasech Isaac <kasechisaac@Kasechs-MacBook-Air.local>
2020-05-15 18:03:29 -0700

    almost

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,13 @@ myFunction();
 
 // Explanation: 
 
+// because closure gives us the ability to put function together.
 
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+function sumation(x){
+return x;
+}
+const add=sumation(4);
+console.log(add(1+2+3));
